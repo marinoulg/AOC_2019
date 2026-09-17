9840c0d93582a249832bbb2686b43f2d8a1f2291
marine le gall <marine.legall@alumni.esade.edu>
2026-09-17 21:30:52 +0200

    finished day 3

--- day3.py
+++ day3.py
@@ -1,28 +1,5 @@
-input = open("exday3.txt", "r").read().split("\n")
-instr1 = input[0].split(",")
-instr2 = input[1].split(",")
+input = open("day3.txt", "r").read().split("\n")
 
-
-
-O = (1,1)
-
-def end_location(instr, where_am_i):
-    for elem in instr:
-        if elem.startswith("R"):
-            where_am_i = (where_am_i[0]+int(elem[1:]), where_am_i[1])
-        elif elem.startswith("L"):
-            where_am_i = (where_am_i[0]-int(elem[1:]), where_am_i[1])
-
-        elif elem.startswith("U"):
-            where_am_i = (where_am_i[0], where_am_i[1]+int(elem[1:]))
-        elif elem.startswith("D"):
-            where_am_i = (where_am_i[0], where_am_i[1]-int(elem[1:]))
-    return (where_am_i)
-
-# print("end instr1", end_location(instr1, O))
-# print("end instr2", end_location(instr2, O))
-
-where_am_i = (1,1)
 
 def stocking_instructions(instr, where_am_i):
     stock = []
@@ -39,7 +16,6 @@ def stocking_instructions(instr, where_am_i):
                 stock.append(where_am_i)
             # print(where_am_i)
 
-
         elif elem.startswith("U"):
             for _ in range(int(elem[1:])):
                 where_am_i = (where_am_i[0], where_am_i[1]+1)
@@ -52,66 +28,9 @@ def stocking_instructions(instr, where_am_i):
                 stock.append(where_am_i)
             # print(where_am_i)
 
-    return ((sorted(list(set(stock))))),where_am_i
+    # print(stock)
+    return (stock),where_am_i
     # return (stock),where_am_i
-
-stock1,where_am_i1 = stocking_instructions(instr1, O)
-stock2,where_am_i2 = stocking_instructions(instr2,O)
-
-# # s1 = stock1.sort(key=lambda tup: tup[1], reverse=True)
-
-print(instr1)
-print(stock1)
-print()
-print(instr2)
-print(stock2)
-
-# similar_coordinates = []
-
-# if stock1[0][0] < stock2[0][0]:
-#     stock_small = (stock1.copy())
-#     stock_big = (stock2.copy())
-# else:
-#     stock_small = (stock2.copy())
-#     stock_big = (stock1.copy())
-
-
-# while stock_small[0][0]!=stock_big[0][0]:
-#     stock_small.pop(0)
-#     stock_small = sorted(stock_small)
-
-print("------")
-# print(stock_small)
-# print()
-# print(stock_big)
-
-
-# for elem1 in sorted(stock_small):
-#     elem2 = sorted(stock_big)[0]
-#     # it_1+=1
-#     while elem1[0] == elem2[0]:
-#         for elem2 in sorted(stock_big):
-#             print(elem1, elem2)
-#             if elem2 == elem1:
-#                 similar_coordinates.append(elem1)
-#             elif elem2[0]>elem1[0]:
-#                 break
-#             else:
-#                 # it_2 +=1
-#                 next
-
-
-
-# print("------")
-# # print(it_1, it_2)
-# print(stock_small)
-# print()
-# print(stock_big)
-
-
-# by batches
-batch1 = []
-old_a = 1
 
 def create_dict_for_stocks(stock):
     mydict = {}
@@ -126,25 +45,81 @@ def create_dict_for_stocks(stock):
 
     return (mydict)
 
-mydict1 = create_dict_for_stocks(stock1)
-mydict2 = create_dict_for_stocks(stock2)
+def comparing_dict_keys(mydict1,mydict2):
+    # comparing dict keys both ways
+    tmp1 = []
+    for elem in (list(mydict1.keys())):
+        if elem not in (list(mydict2.keys())):
+            tmp1.append(elem)
+    tmp2 = []
+    for elem in (list(mydict2.keys())):
+        if elem not in (list(mydict1.keys())):
+            tmp2.append(elem)
+    return tmp1, tmp2
 
-# comparing dict keys both ways
-tmp1 = []
-for elem in (list(mydict1.keys())):
-    if elem not in (list(mydict2.keys())):
-        tmp1.append(elem)
-tmp2 = []
-for elem in (list(mydict2.keys())):
-    if elem not in (list(mydict1.keys())):
-        tmp2.append(elem)
+def deleting_useless_keys(mydict1,mydict2,
+                          tmp1,tmp2):
+    # deleting useless keys both ways
+    for elem in tmp1:
+        mydict1.pop(elem)
 
-for elem in tmp1:
-    mydict1.pop(elem)
+    for elem in tmp2:
+        mydict2.pop(elem)
+    return mydict1, mydict2
 
-for elem in tmp2:
-    mydict2.pop(elem)
+def manhattan_distance(coords1, coords2):
+        x1, y1 = coords1
+        x2, y2 = coords2
+        return abs(x1-x2)+abs(y1-y2)
 
-print(mydict1)
+def part1(input):
+    O = (1,1)
+    where_am_i = (1,1)
+
+    instr1 = input[0].split(",")
+    instr2 = input[1].split(",")
+    stock1,where_am_i1 = stocking_instructions(instr1, O)
+    stock2,where_am_i2 = stocking_instructions(instr2,O)
+
+    mydict1 = create_dict_for_stocks(stock1)
+    mydict2 = create_dict_for_stocks(stock2)
+    tmp1, tmp2 = comparing_dict_keys(mydict1,mydict2)
+    mydict1,mydict2 = deleting_useless_keys(mydict1,mydict2,tmp1,tmp2)
+
+    # print(instr1)
+    # print(stock1)
+    # print()
+    # print(instr2)
+    # print(stock2)
+
+    similar_coordinates = []
+    for key in mydict1.keys():
+        for elem1 in (mydict1[key]):
+            for elem2 in mydict2[key]:
+                if elem1 == elem2:
+                    similar_coordinates.append(elem1)
+
+    distances = {}
+    for coord in similar_coordinates:
+        distances[(manhattan_distance(O,coord))] = coord
+
+    return stock1, stock2,distances
+
 print()
-print(mydict2)
+stock1, stock2,distances = (part1(input))
+print("Answer part 1:", sorted(list(distances.keys()))[0])
+print()
+
+def counting_iterations_to_get_to_cross(stock, coordinate):
+    it = 1
+    for elem in (stock):
+        if elem != coordinate:
+            it +=1
+        else: break
+    return it
+
+steps = []
+for elem in (list(distances.values())):
+    steps.append(counting_iterations_to_get_to_cross(stock1,elem)+counting_iterations_to_get_to_cross(stock2,elem))
+
+print(sorted(steps)[0])
